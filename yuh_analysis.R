db2acddd1dfdae364ab333dd73134d236573be1c
Yuhaniz <ayuhaniz00@gmail.com>
2021-05-11 19:50:57 -0700

    added analysis, lint

--- yuh_analysis.R
+++ yuh_analysis.R
@@ -22,7 +22,7 @@ df_edit <- na_if(df, "") %>%
 homicide_plot <- ggplot(df_edit) +
   geom_point(mapping = aes(x = rate_black,
                            y = rate_white,
-                           color=State)) +
+                           color = State)) +
   theme(legend.position = "none") +
   geom_abline(intercept = 0, slope = 1) +
   coord_cartesian(xlim = c(0, 30), ylim = c(0, 30)) +
@@ -35,3 +35,14 @@ homicide_plot <- ggplot(df_edit) +
 
 #Interactive scatter plot
 homicide_plot_interactive <- ggplotly(homicide_plot)
+
+#Analysis
+#This scatter plot shows that the average annual police homicide rate between
+#Black and White people are not proportional. All the points, or states, under
+#the line means that the average annual police homicide rate is greater among
+#Black people than White people. The points above the line means that police
+#homicide rate is greater among White people than Black people. The graph shows
+#that three states, Hawaii, Kansas, and Alabama, have higher rates of police
+#homicide among White people than Black people. However, the majority of states
+#have much more incidents of higher rates of police homicide for Black people,
+#as well as a rate of a higher degree.
