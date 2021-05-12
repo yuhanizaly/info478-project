fa79cae8430f366daa5ea613c2d9b14e981d8e8b
Dainese Chandra <dainesechandra@Daineses-MacBook-Air.local>
2021-05-12 12:13:29 -0700

    3 added

--- dain_analysis.R
+++ dain_analysis.R
@@ -8,7 +8,7 @@ victim_race_data <- read.csv("police_killings_MPV.csv")
 victim_race_data <- victim_race_data %>%
   select(Victim.s.race, Victim.s.gender, Date.of.Incident..month.day.year.,
          City, State) %>%
-  mutate(year = substring(Date.of.Incident..month.day.year.,7,10))
+  mutate(year = substring(Date.of.Incident..month.day.year., 7, 10))
 
 
 #TOTAL
@@ -19,10 +19,13 @@ totalvictims_race <- victim_race_data %>%
   mutate(proportion = totalnum / sum(totalnum))
 
 totalprop_race <- ggplotly(ggplot(data = totalvictims_race,
-                                  aes(x = Victim.s.race, y = proportion, fill = Victim.s.race)) +
+                                  aes(x = Victim.s.race, y = proportion,
+                                      fill = Victim.s.race)) +
                              geom_bar(stat = "identity") +
-                             theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-                             labs(title = "Proportion of Victims in Police Killings by Race from 2013-2020"))
+                             theme(axis.text.x = element_text(angle = 90,
+                                                              hjust = 1)) +
+                             labs(title = "Proportion of Victims in Police
+                             Killings by Race from 2013-2020"))
 
 #From the data given we can see that the top 3 races of victims in police
 #killings from 2013 to 2020 are White (44%), Black (25.2%) and Hispanic (17.2%).
@@ -34,9 +37,9 @@ totalprop_race <- ggplotly(ggplot(data = totalvictims_race,
 #BY YEAR (eg. 2020)
 
 #function for distribution of race of victims in a year
-proportion_year_fn <- function(yearUsed) {
+proportion_year_fn <- function(year_used) {
   prop_by_year <- victim_race_data %>%
-    filter(year == yearUsed) %>%
+    filter(year == year_used) %>%
     group_by(Victim.s.race) %>%
     summarise(totalnum = n()) %>%
     mutate(proportion = totalnum / sum(totalnum))
@@ -44,7 +47,8 @@ proportion_year_fn <- function(yearUsed) {
 }
 
 plot_2020 <- ggplotly(ggplot(data = proportion_year_fn(2020),
-                    aes(x = Victim.s.race, y = proportion, fill = Victim.s.race)) +
+                    aes(x = Victim.s.race, y = proportion,
+                        fill = Victim.s.race)) +
   geom_bar(stat = "identity") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   labs(title = "Proportion of Victims in Police Killings by Race in 2020"))
@@ -57,8 +61,7 @@ killings_per_year <- victim_race_data %>%
   summarise(total_inyear = n()) %>%
   mutate(mean = mean(total_inyear)) %>%
   mutate(sd = sd(total_inyear))
-#Mean = 1053.375
-#sd = 113.3095
 
 
 
+#Mean = 1053.375, sd = 113.3095
